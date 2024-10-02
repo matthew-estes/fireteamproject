0dba4274584b68cfe3de597af2de00b35f1d32fc
tflinch <tflinch16@gmail.com>
2024-10-02 11:20:30 -0400

    feat: add function user.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODBURI);
+//import models
+
+const User = require('./user');
+
 const db = mongoose.connection;
 
-db.on('connected', function () {
-    console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
-});
+db.once('open', () =>
+  console.log(`Connected to MongoDB at ${db.host}.${db.port}`)
+);
+db.on('error', (error) => console.log('Database error \n', error));
 
 module.exports = {
-    Test: require('./test'),
-}
+  //models export
+  User,
+  Test: require('./test'),
+};
