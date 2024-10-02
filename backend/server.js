1aa162ee4f55cbd2d65110a7ba4f26213cfaa3dc
tflinch <tflinch16@gmail.com>
2024-10-02 11:08:50 -0400

    feat: add packages express-session, passport-config

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
 // External requires
 
-require("dotenv").config();
-const path = require("path");
-const express = require("express");
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const passport = require('./config/passport-config');
 let livereload = undefined;
 let connectLivereload = undefined;
-if (process.env.ON_HEROKU === "false") {
-    console.log("processing dev-only require() statements...");
-    livereload = require("livereload");
-    connectLivereload = require("connect-livereload");
+if (process.env.ON_HEROKU === 'false') {
+  console.log('processing dev-only require() statements...');
+  livereload = require('livereload');
+  connectLivereload = require('connect-livereload');
 }
 
 // Internal requires
 
-const testController = require("./controllers/test");
-const models = require("./models/index");
-    const testModel = models.testModel;
+const testController = require('./controllers/test');
+const models = require('./models/index');
+const testModel = models.testModel;
+
+const SECRET_SESSION = process.env.SECRET_SESSION;
+
+// initial passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Usual express.js setup
 
@@ -27,16 +35,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(path.dirname(__dirname), 'frontend', 'dist')));
 
 let livereloadServer = undefined;
-if (process.env.ON_HEROKU === "false") {
-    console.log("Processing dev-only liveload and connect-livereload configuration.");
-    livereloadServer = livereload.createServer();
-    livereloadServer.server.once("connection", () => {
-        setTimeout(() => {
-            livereloadServer.refresh("/");
-        }, 100);
-    });
-    app.use(connectLivereload());
+if (process.env.ON_HEROKU === 'false') {
+  console.log(
+    'Processing dev-only liveload and connect-livereload configuration.'
+  );
+  livereloadServer = livereload.createServer();
+  livereloadServer.server.once('connection', () => {
+    setTimeout(() => {
+      livereloadServer.refresh('/');
+    }, 100);
+  });
+  app.use(connectLivereload());
 }
+
+// middleware for tracking users and alerts
+app.use((req, res, next) => {
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next(); // going to said route
+});
 
 // Mount controllers
 
@@ -44,17 +61,21 @@ app.use('/api/test', testController);
 
 // Non-REST routes
 
-app.get("/api", (req, res) => {
-    res.send("This message was emitted by the backend.");
+app.get('/api', (req, res) => {
+  res.send('This message was emitted by the backend.');
 });
 
 // Any other route not matching the routes above gets routed by React
 app.get('*', (req, res) => {
-    res.sendFile(path.join(path.dirname(__dirname), 'frontend', 'dist', 'index.html'));
+  res.sendFile(
+    path.join(path.dirname(__dirname), 'frontend', 'dist', 'index.html')
+  );
 });
 
 // Start server
 
 app.listen(process.env.PORT, () => {
-    console.log(`Express is listening on port ${process.env.PORT}.`);
+  console.log(`Express is listening on port ${process.env.PORT}.`);
 });
+
+module.exports = server;
