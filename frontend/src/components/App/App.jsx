ee9eae4887155d981155519e5bdc29c00521f0c1
matthew-estes <98664968+matthew-estes@users.noreply.github.com>
2024-10-02 10:29:15 -0400

    feat: add weather-card layout

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react';
+import { Routes, Route, useNavigate } from "react-router-dom";
 import './App.css';
 
 function App() {
 
 
   return (
-   <>
-   Hello World
-   </>
-  );
+    <div className="weather-card">
+    <div className="location-info">
+      <h1>My Location</h1>
+      <p>Los Angeles</p>
+    </div>
+    <div className="temperature">
+      <span>00°</span>
+    </div>
+    <div className="temp-range">
+      <p>Low: 00° | High: 00°</p>
+    </div>
+  </div>
+);
 }
+
 
 export default App;
