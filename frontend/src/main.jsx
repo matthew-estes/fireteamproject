ceed1c3258b3fee3caee85b9f0ebcc863c0c6f07
matthew-estes <98664968+matthew-estes@users.noreply.github.com>
2024-10-02 10:15:06 -0400

    feat: create App.jsx and App.css, remove TestPage component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App'
+import App from './components/App/App.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
