6c92837f08e896e5d419b328bb8f0ad3d9ff3bad
matthew-estes <98664968+matthew-estes@users.noreply.github.com>
2024-10-05 15:17:35 -0400

    update app name to Weather Section, center, add 30px to the margin top, add search icon to be incorporated at a later date

--- frontend/src/components/TopAppBar/TopAppBar.jsx
+++ frontend/src/components/TopAppBar/TopAppBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
 import "./TopAppBar.css";
 
-function TopAppBar({ toggleDrawer }) {
+function TopAppBar({ toggleDrawer, onSearchClick }) { 
   return (
     <AppBar
       position="static"
@@ -13,13 +14,27 @@ function TopAppBar({ toggleDrawer }) {
         boxShadow: "none",
       }}
     >
-      <Toolbar>
+     <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '30px' }}> 
         <IconButton edge="start" color="inherit" onClick={toggleDrawer} aria-label="menu">
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" sx={{flexGrow: 1, fontFamily: "Merriweather, serif"}}>
-          The Weather Section
+        <Typography
+          variant="h6"
+          sx={{
+            flex: 1,
+            textAlign: 'center',
+            fontFamily: "Merriweather, serif"
+          }}
+        >
+          Weather Section
         </Typography>
+        <IconButton
+          edge="end" 
+          color="inherit"
+          onClick={onSearchClick}
+        >
+          <SearchIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
